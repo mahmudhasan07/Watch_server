412f224f78ba12fb7cf9e9d4b6114c819e9a8157
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-09 22:19:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+const port = process.env.PORT || 3000
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/", async(req,res)=>{
+    res.send("Welcome to the API server")
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`The server is running at ${port}`);
+})
