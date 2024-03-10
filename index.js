523f50c0680dc783ad52a0a07ef9ecb6bc98f76f
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-11 01:45:38 +0600

    Added mysql data

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ const express = require("express")
 const cors = require("cors")
 const port = process.env.PORT || 3000
 const app = express()
+var mysql = require('mysql');
 app.use(cors())
 app.use(express.json())
 
-app.get("/", async(req,res)=>{
+app.get("/", async (req, res) => {
     res.send("Welcome to the API server")
 })
 
-app.get("/products", async(req,res)=>{
-    res.send("no data")
+var con = mysql.createConnection({
+    host: process.env.Host_API,
+    user: process.env.User_ID,
+    password: process.env.User_Password
+});
+
+con.connect(function (err) {
+    if (err) return err;
+    console.log("Connected!");
+});
+
+
+app.post("/products", async (req, res) => {
+    const data = req.body
+
 })
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`The server is running at ${port}`);
 })
