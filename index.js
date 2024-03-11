14e04b01240a928a51500c824fff8ed74371932b
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-12 05:16:33 +0600

    create new route

--- index.js
+++ index.js
@@ -4,29 +4,52 @@ const port = process.env.PORT || 3000
 const app = express()
 require('dotenv').config()
 var mysql = require('mysql');
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json())
 
 app.get("/", async (req, res) => {
     res.send("Welcome to the API server")
 })
 
-console.log(process.env.Host_API);
 
 var con = mysql.createConnection({
     host: process.env.Host_API,
     user: process.env.User_ID,
-    password: process.env.User_Password
+    password: process.env.User_Password,
+    database: process.env.User_DataBase
 });
 
-con.connect(function (err) {
-    if (err) return err;
-    console.log("Connected!");
-});
+
+
+app.get("/watches", async(req,res)=>{
+    const sql = "SELECT * FROM watch"
+    con.query(sql,(error, data)=>{
+        res.send(data)
+        if(error){
+            res.send("error")
+        }
+    })
+})
 
 
 app.post("/products", async (req, res) => {
     const data = req.body
+    console.log(data);
+    // database er name alz single rakba
+    const sql = "INSERT INTO watch(`Watch_name`,`Watch_brand`,`Watch_image`,`Watch_gender`,`Watch_price`,`Watch_note`)  VALUES(?)"
+    if (data) {
+        const values = [data?.name, data?.brand, data?.imgURL, data?.gender, data?.price, data?.note]
+        con.query(sql, [values], (error, result)=> {
+            if (error) {
+              return  res.send("error paise")
+            }
+            res.send("successful")
+
+        })
+    }
 
 })
 
