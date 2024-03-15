690a078c38d2937015ec681c44e7a03a327e23d9
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-16 01:24:08 +0600

    solve search filter problem

--- index.js
+++ index.js
@@ -24,28 +24,29 @@ var con = mysql.createConnection({
 
 
 
-app.get("/watches", async(req,res)=>{
+app.get("/watches", async (req, res) => {
     const id = req.query
     console.log(id);
-    let sql 
-    if(id.data){
-       sql= `SELECT * FROM watch ORDER BY Watch_price ${id?.data}`
-    }else{
-        sql= `SELECT * FROM watch`
+    let sql
+    if (id.data) {
+        sql = `SELECT * FROM watch ORDER BY Watch_price ${id?.data}`
+    } else {
+        sql = `SELECT * FROM watch`
     }
-    con.query(sql,(error, data)=>{
+    con.query(sql, (error, data) => {
         res.send(data)
-        if(error){
+        if (error) {
             res.send("error")
         }
     })
 })
 
-app.get("/search/:brand", async(req,res)=>{
+app.get("/search/:brand", async (req, res) => {
     const data = req.params
-    const sql =`SELECT * FROM watch WHERE Watch_brand = ${data?.brand}`
-    con.query(sql,(err,result)=>{
-        console.log(result);
+    const sql = `SELECT * FROM watch WHERE Watch_brand = '${data?.brand}'`
+    con.query(sql, (err, result) => {
+        res.send(result)
+        
     })
     console.log(data);
 })
@@ -58,9 +59,9 @@ app.post("/products", async (req, res) => {
     const sql = "INSERT INTO watch(`Watch_name`,`Watch_brand`,`Watch_image`,`Watch_gender`,`Watch_price`,`Watch_note`)  VALUES(?)"
     if (data) {
         const values = [data?.name, data?.brand, data?.imgURL, data?.gender, data?.price, data?.note]
-        con.query(sql, [values], (error, result)=> {
+        con.query(sql, [values], (error, result) => {
             if (error) {
-              return  res.send("error paise")
+                return res.send("error paise")
             }
             res.send("successful")
 
